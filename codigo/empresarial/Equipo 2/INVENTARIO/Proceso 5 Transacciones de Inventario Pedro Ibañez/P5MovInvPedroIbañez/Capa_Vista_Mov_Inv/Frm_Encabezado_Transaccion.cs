aa0aafa4b9eac70556ca4689de0db0f77deca52a
Pedro Ibañez <pibanezl@miumg.edu.gt>
2026-04-08 19:26:51 -0600

    Vista Formulario Movimientos Inventario

---
+++ codigo/empresarial/Equipo 2/INVENTARIO/Proceso 5 Transacciones de Inventario Pedro Ibañez/P5MovInvPedroIbañez/Capa_Vista_Mov_Inv/Frm_Encabezado_Transaccion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Vista_Mov_Inv
+{
+    public partial class Frm_Encabezado_Transaccion : Form
+    {
+        public Frm_Encabezado_Transaccion()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- codigo/empresarial/Equipo 2/INVENTARIO/Proceso 5 Transacciones de Inventario Pedro Ibañez/P5MovInvPedroIbañez/Capa_Vista_Mov_Inv/Frm_Mantenimiento_Tipo_Op_CXC.Designer.cs
+++ codigo/empresarial/Equipo 2/INVENTARIO/Proceso 5 Transacciones de Inventario Pedro Ibañez/P5MovInvPedroIbañez/Capa_Vista_Mov_Inv/Frm_Mantenimiento_Tipo_Op_CXC.Designer.cs
@@ -30,13 +30,16 @@ namespace Capa_Vista_Mov_Inv
         private void InitializeComponent()
         {
             this.navegador1 = new Capa_Vista_Navegador.Navegador();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // navegador1
             // 
             this.navegador1.IPkId_Aplicacion = 0;
             this.navegador1.IPkId_Modulo = 0;
-            this.navegador1.Location = new System.Drawing.Point(13, 13);
+            this.navegador1.Location = new System.Drawing.Point(12, 55);
             this.navegador1.Name = "navegador1";
             this.navegador1.SAlias = null;
             this.navegador1.SEtiquetas = null;
@@ -44,14 +47,36 @@ namespace Capa_Vista_Mov_Inv
             this.navegador1.SNombreTabla = null;
             this.navegador1.TabIndex = 0;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1212, 52);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(681, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MANTENIMIENTO TIPO OPERACION CUENTA POR COBRAR";
+            // 
             // Frm_Mantenimiento_Tipo_Op_CXC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1148, 555);
+            this.ClientSize = new System.Drawing.Size(1211, 599);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.navegador1);
             this.Name = "Frm_Mantenimiento_Tipo_Op_CXC";
-            this.Text = "Frm_Mantenimiento_Tipo_Op_CXC";
+            this.Text = "Mantenimiento Tipo Cuentas por Cobrar";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +84,7 @@ namespace Capa_Vista_Mov_Inv
         #endregion
 
         private Capa_Vista_Navegador.Navegador navegador1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
